b7ea7bc61e2e59789a348685c690d15e8198f9eb
Matthew Huskey <matthewhuskey@me.com>
2018-07-16 23:02:05 -0400

    Log In & Log Out
    • Users can now log in and log out
    • Signing up automatically signs in the user
    • Added tests for logging in and logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,18 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(id: session[:user_id])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
-      # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
   
-  def destory
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
