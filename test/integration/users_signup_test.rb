0a59fa47017edd23fc0b5eb410b6f46a72367429
Matthew Huskey <matthewhuskey@me.com>
2018-08-05 17:15:03 -0400

    Destroying Microposts
    • Added a link to delete microposts for the current user
    • Added an action to destroy selected micropost with a before_action to verify the micropost exists
    • Added a controller test and integration test to verify new behavior works correctly

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,12 +27,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # Try to log in before activation.
     log_in_as(user)
     assert_not is_logged_in?
+    
     # Invalid activation token
     get edit_account_activation_path("invalid token", email: user.email)
     assert_not is_logged_in?
+    
     # Valid token, wrong email
     get edit_account_activation_path(user.activation_token, email: 'wrong')
     assert_not is_logged_in?
+    
     # Valid activation token
     get edit_account_activation_path(user.activation_token, email: user.email)
     assert user.reload.activated?
