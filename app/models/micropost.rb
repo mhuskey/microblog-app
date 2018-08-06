3eda65ba73f118c5933ff86dcecb84505f43604c
mhuskey <matthewhuskey@me.com>
2018-08-05 22:52:25 -0400

    Image Validation
    • Added validation preventing the uploading of images larger than five megabytes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,4 +9,16 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 280 }
   
+  validate  :picture_size
+  
+  
+  private
+    
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+  
 end
